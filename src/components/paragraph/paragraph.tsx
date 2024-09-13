7aafca64bcbe72afe9edb54d37dc9ed92eb1e3d5
montanari2019 <ikaro.montanari31@gmail.com>
2024-09-13 19:07:18 -0400

    corrigindo bug do paragrapho

--- src/components/paragraph/paragraph.tsx
+++ src/components/paragraph/paragraph.tsx
@@ -1,29 +1,37 @@
+
+
 import { ParagraphModelProps } from "@/models/paragraph.model";
 import styled from "../../styles/paragraph/paragraph.module.css";
+import { randomUUID } from "crypto";
 export function Paragraph({
   description,
   textAlignment,
   textWeight = "normal",
   fontSize = "--FONT-SIZE-SM",
 }: ParagraphModelProps) {
+  const lines = description.split("\n").map((line) => {
+    return {
+      line: line,
+      id: Math.random().toString(36),
+    };
+  });
   return (
     <div className={styled.container}>
-      <p
-        style={{
-          textAlign: textAlignment,
-          fontWeight: textWeight,
-          fontSize: `var(${fontSize})`,
-        }}
-      >
-        {description.split("\n").map((line) => {
-          return (
-            <>
-              {line}
-              <br />
-            </>
-          );
-        })}
-      </p>
+      {lines.map((index) => {
+        return (
+          <p
+            key={index.id}
+            style={{
+              textAlign: textAlignment,
+              fontWeight: textWeight,
+              fontSize: `var(${fontSize})`,
+            }}
+          >
+            {index.line}
+            <br />
+          </p>
+        );
+      })}
     </div>
   );
 }
