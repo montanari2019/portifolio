8ccd537d93bf92a363d25be7ddc291b6aa0642cb
montanari2019 <ikaro.montanari31@gmail.com>
2025-02-17 10:26:36 -0400

    implementando melhorias

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,5 +1,5 @@
 export enum URLS_ENUM {
-  WHATSAPP = "https://wa.me/69993569547?text=Ol%C3%A1%2C%20vim%20pelo%20seu%20portf%C3%B3lio.",
+  WHATSAPP = "https://wa.me/5569993569547?text=Ol%C3%A1%2C%20vim%20pelo%20seu%20portf%C3%B3lio.",
   INSTAGRAM = "https://www.instagram.com/ikaro.montanari/",
   LINKEDIN = "https://www.linkedin.com/in/ikaro-montanari-5aa120208/",
   GITHUB = "https://github.com/montanari2019",
