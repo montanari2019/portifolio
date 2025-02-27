b6032c7c7c1e6c6716c9ffb48cf5c91abe2e7a75
montanari2019 <ikaro.montanari31@gmail.com>
2025-02-27 11:28:10 -0400

    implementando novas fotos no site

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,10 +4,30 @@ import { Button } from "@/components/button";
 import { LinksNetwork } from "@/components/linksNetworking/linksNetworking";
 import { URLS_ENUM } from "@/utils/urls";
 import foto from "../assets/foto_profile.png";
+import imageQuadHd from "@/assets/initialSection/DesktopWIDE.svg";
+import imageFullHd from "@/assets/initialSection/DesktopFULLHD.svg";
+import imageMobile from "@/assets/initialSection/iPhone_MOBILE.svg";
 import styled from "../styles/pages/home/screens/home.module.css";
+import Image from "next/image";
+
 export function HomeScreens() {
   return (
     <main className={styled.container}>
+      <section className={styled.imageSection}>
+        {/* <Image fill src={imageQuadHd} alt="Ikaro Montanari" className={styled.fotoQuadHd} /> */}
+        <Image
+          fill
+          src={imageFullHd}
+          alt="Ikaro Montanari"
+          className={styled.fotoFullHd}
+        />
+        <Image
+          fill
+          src={imageMobile}
+          alt="Ikaro Montanari"
+          className={styled.fotoMobile}
+        />
+      </section>
       <div className={styled.containerSections}>
         <section>
           <div className={styled.titles}>
@@ -26,15 +46,6 @@ export function HomeScreens() {
           >
             <Button field title="Contate-me" width={313} />
           </a>
-        </section>
-
-        <section className={styled.containerProfile}>
-          <div className={styled.elipse}></div>
-          <img
-            src={foto.src}
-            className={styled.imageDev}
-            alt="foto do desenvolvedor"
-          />
         </section>
 
         <div className={styled.displayLinks}>
