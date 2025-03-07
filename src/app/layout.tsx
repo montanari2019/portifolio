fb4aa01f5f92f5828a4157cfdeb26da94440bf55
montanari2019 <ikaro.montanari31@gmail.com>
2025-03-07 12:07:28 -0400

    melhorando metadados

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import { AcuminLocalFont } from "@/assets/fonts";
 import type { Metadata } from "next";
 import "../styles/globals.css";
+import favicon from "./favicon.ico";
+import image from "@/assets/imagens/profile_IM.png";
 
 export const metadata: Metadata = {
   title: "Montanari Soft",
+  icons: {
+    icon: favicon.src, // Caminho para o favicon
+    apple: favicon.src, // Ícone para dispositivos Apple (iPhone, iPad)
+  },
   description:
     "A Montanari Soft é uma empresa especializada em soluções tecnológicas inovadoras, oferecendo desenvolvimento de software, integração de APIs e análise de dados com foco em eficiência e qualidade.",
   keywords:
@@ -14,6 +20,10 @@ export const metadata: Metadata = {
       "A Montanari Soft é uma empresa especializada em soluções tecnológicas inovadoras, oferecendo desenvolvimento de software, integração de APIs e análise de dados com foco em eficiência e qualidade.",
     type: "website",
     url: "https://montanarisoft.com.br/",
+    siteName: "Montanari Soft",
+    emails: "montanarisoftoficial@gmail.com",
+    phoneNumbers: "69993569547",
+    images: [image.src],
   },
 };
 
@@ -24,6 +34,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href={favicon.src} sizes="any" />
+      </head>
       <body className={AcuminLocalFont.className}>{children}</body>
     </html>
   );
